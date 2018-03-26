21b66b4ec9996697ed52d131d61cd1b9d8601e70
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-25 18:45:35 -0600

    Connecting with API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <p>Front End Project: Mallory Furniture</p>
-      </div>
-    );
-  }
+  
+	fetchFurniture = product => {
+		request
+			.get(`https://mallory-furniture-admin.now.sh/api/v1/products`)
+			.then(response => {
+				console.log('---------------')
+				console.log(response.body)
+				console.log('---------------')	
+			})
+	}
+
+	render() {
+  		this.fetchFurniture()
+	    return (
+	      <div>
+	          <h2>Front End Project:</h2>
+	          <h2>Mallory Furniture</h2>
+	          <h2>See console</h2>
+	      </div>
+	    );
+	}
 }
 
 export default App;

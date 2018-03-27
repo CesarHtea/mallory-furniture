1ad741f5dc1853f7663357fc553f11c8ee2a9b98
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-27 08:45:48 -0600

    Preparing files for new function

--- src/components/Furniture.js
+++ src/components/Furniture.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import request from 'superagent';
+import ItemCard from './ItemCard';
 
 class Furniture extends Component {
+
 	constructor(props) {
 		super();
-
 		this.state = {
 			data: []
-		};		
-		
-		
-	}
+		};					
+		console.log(this.state.data)
+	} 
 
 	componentDidMount() {
-    console.log(this.props.match.params.category)
-    request
-      .get(`https://mallory-furniture-admin.now.sh/api/v1/products`)
-      .then(response => {
-      	
-        this.setState({
-          data: response.body
-        })
-      });
+		console.log(this.props.match.params.category)
+	    request
+	      .get(`https://mallory-furniture-admin.now.sh/api/v1/products`)
+	      .then(response => {
+	      	  this.setState({
+	          data: response.body
+	      	  })
+	      	  console.log(this.state.data)
+	      });
   	}
   	
+  	funcionFiltro() {
+  		console.log("hola")
+  	}
 
   	render() {
+		this.funcionFiltro()
 		return (
 			<div>
 				<h2>Category</h2>
+				{this.state.data.map(function(product, i){
+					return (
+						<ItemCard item={product.item} image={product.imageLink} />	
+						);
+				})}
 			</div>
 		);
 	}

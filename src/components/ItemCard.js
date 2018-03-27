5a8f379ba523d5cb698b3313fd571f8bfbad39be
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-27 16:15:32 -0600

    Add dynamic URL/Render to each rendered item (product)

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ItemCard extends Component {
 
   render() {
 		const product = this.props
+		const URL = '/products/'+ product.id
 		console.log(product.id)
 		return (
 			<div>
+			  <Link to={URL}>
 				<span>
 					<p className='main'> Item: {product.item} </p>
 					<img className='thumb' src={product.image} alt='' />
-					<p className='main'>Item Id: {product.id} </p>
 				</span>
+			  </Link>
 			</div>	
 		)
 	}

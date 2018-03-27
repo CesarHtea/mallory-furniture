add31a813d6eb676dad72ef1ce5db318e0943588
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-27 10:58:50 -0600

    Filter and map in Furniture.js

--- src/components/Furniture.js
+++ src/components/Furniture.js
@@ -4,44 +4,58 @@ import ItemCard from './ItemCard';
 
 class Furniture extends Component {
 
-	constructor(props) {
-		super();
-		this.state = {
-			data: []
-		};					
-		console.log(this.state.data)
-	} 
+  constructor(props) {
+    super();
+    this.state = {
+      data: []
+    };					
+    // console.log(this.state.data)
+  } 
 
-	componentDidMount() {
-		console.log(this.props.match.params.category)
-	    request
-	      .get(`https://mallory-furniture-admin.now.sh/api/v1/products`)
-	      .then(response => {
-	      	  this.setState({
-	          data: response.body
-	      	  })
-	      	  console.log(this.state.data)
-	      });
-  	}
-  	
-  	funcionFiltro() {
-  		console.log("hola")
-  	}
+  componentDidMount() {
+    request
+      .get(`https://mallory-furniture-admin.now.sh/api/v1/products`)
+      .then(response => {
+        this.setState({
+          data: response.body
+        })
+      });
+  }
 
-  	render() {
-		this.funcionFiltro()
-		return (
-			<div>
-				<h2>Category</h2>
-				{this.state.data.map(function(product, i){
-					return (
-						<ItemCard item={product.item} image={product.imageLink} />	
-						);
-				})}
-			</div>
-		);
-	}
+  	// filtro segun url
+  	// retorna una lista 
+  	// lista.map
+
+  createList = () => {
+  	let category = this.props.match.params.category
+  	// console.log(category)
+    return this.state.data.filter( mueble => mueble.category === category );
+  }
+
+  // createList = () => {
+  // 	let category = this.props.match.params.category
+  // 	console.log(category)
+  //   return this.state.data.filter(function(mueble) {
+  //     if (mueble.category === category) return true;
+  //   });
+  // }
+
+
+  render() {
+    const newList = this.createList();
+    // console.log(newList)
+    return (
+      <div>
+      <h2>Category</h2>
+      {newList.map(function(product,i ){
+        return (
+          <ItemCard item={product.item} image={product.imageLink} />	
+	    );
+      })}
+	  </div>
+    );
+  }
+
 }
-
 
 export default Furniture;
